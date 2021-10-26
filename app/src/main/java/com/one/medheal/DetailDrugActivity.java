a5862ed696d8ce778b835791ed213e827cbe7dd1
arianurhadi <arianurhadi81@gmail.com>
2021-10-26 23:14:07 +0700

    update layout

---
+++ app/src/main/java/com/one/medheal/DetailDrugActivity.java
@@ -0,0 +1,14 @@
+package com.one.medheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailDrugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_drug);
+    }
+}
