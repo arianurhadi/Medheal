c8554b51e3b799bbc549b971fb4b4dfb92afa47a
“Reggy <Reggypsulaeman@gmail.com>
2021-10-28 20:42:08 +0700

    onboarding

---
+++ app/src/main/java/com/one/medheal/Onboarding.java
@@ -0,0 +1,14 @@
+package com.one.medheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Onboarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
