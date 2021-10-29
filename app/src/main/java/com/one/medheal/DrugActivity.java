fe1ba47379362bc41fb1cadb182d5c8c09484b53
arianurhadi <arianurhadi81@gmail.com>
2021-10-29 14:44:40 +0700

    update 0.0.1

--- app/src/main/java/com/one/medheal/DrugActivity.java
+++ app/src/main/java/com/one/medheal/DrugActivity.java
@@ -2,13 +2,60 @@ package com.one.medheal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DrugActivity extends AppCompatActivity {
+
+    boolean visible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drug);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
+        LinearLayout bottomSubMenu = findViewById(R.id.bottomSubmenu);
+        LinearLayout btnSymptom = findViewById(R.id.btnSymptom);
+
+        bottomNavigationView.setSelectedItemId(R.id.drug);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    Intent intent = new Intent(DrugActivity.this, HomeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    break;
+                case R.id.drug:
+                    Intent intent2 = new Intent(DrugActivity.this, DrugActivity.class);
+                    intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent2);
+                    break;
+                case R.id.track:
+                    if (visible){
+                        bottomSubMenu.setVisibility(View.VISIBLE);
+                        visible = false;
+                    } else {
+                        bottomSubMenu.setVisibility(View.GONE);
+                        visible = true;
+                    }
+                    break;
+            }
+
+            return false;
+        });
+
+        btnSymptom.setOnClickListener(view -> {
+            Intent intent = new Intent(DrugActivity.this, SymptomActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+        });
+
     }
 }
--- app/src/main/java/com/one/medheal/HomeActivity.java
+++ app/src/main/java/com/one/medheal/HomeActivity.java
@@ -8,15 +8,23 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    boolean visible = true;
 
     @SuppressLint("WrongConstant")
     @Override
@@ -24,21 +32,57 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
+        LinearLayout bottomSubMenu = findViewById(R.id.bottomSubmenu);
+        LinearLayout btnSymptom = findViewById(R.id.btnSymptom);
         AppCompatButton btnSidebar = findViewById(R.id.btnSidebar);
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
         NavigationView navView = findViewById(R.id.navView);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
         btnSidebar.setOnClickListener(view -> {
             drawerLayout.openDrawer(START);
         });
 
-        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                item.setChecked(true);
-                drawerLayout.closeDrawer(drawerLayout);
-                return true;
+        navView.setNavigationItemSelectedListener(item -> {
+            item.setChecked(true);
+            drawerLayout.closeDrawer(drawerLayout);
+            return true;
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    break;
+                case R.id.drug:
+                    Intent intent2 = new Intent(HomeActivity.this, DrugActivity.class);
+                    intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent2);
+                    break;
+                case R.id.track:
+                    if (visible){
+                        bottomSubMenu.setVisibility(View.VISIBLE);
+                        bottomSubMenu.animate().translationY(20);
+                        visible = false;
+                    } else {
+                        bottomSubMenu.setVisibility(View.GONE);
+                        visible = true;
+                    }
+                    break;
             }
+
+            return false;
+        });
+
+        btnSymptom.setOnClickListener(view -> {
+            Intent intent = new Intent(HomeActivity.this, SymptomActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
         });
 
     }
