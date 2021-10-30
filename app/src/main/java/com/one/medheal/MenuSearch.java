d917903ae887e3597cc04c24d1ca58cf52fa25b3
Mohd Yusuf Ramdhani <60150914+ucup990@users.noreply.github.com>
2021-10-30 09:07:32 +0700

    Add files via upload

---
+++ app/src/main/java/com/one/medheal/MenuSearch.java
@@ -0,0 +1,14 @@
+package com.one.medheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_search);
+    }
+}
