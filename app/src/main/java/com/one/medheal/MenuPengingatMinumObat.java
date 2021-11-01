8d18cd7863b15e408aa444de7f3b176b41e5d068
Mohd Yusuf Ramdhani <60150914+ucup990@users.noreply.github.com>
2021-11-01 08:48:47 +0700

    Add files via upload

---
+++ app/src/main/java/com/one/medheal/MenuPengingatMinumObat.java
@@ -0,0 +1,14 @@
+package com.one.medheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPengingatMinumObat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_pengingat_minum_obat);
+    }
+}
